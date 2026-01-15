0b89343602ecc74a580d1f124c81d7155a66d763
bwu32 <bwu32@terpmail.umd.edu>
2026-01-15 02:30:15 -0500

    hi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // <--- This creates the ultra-fast production build
+  /* your other config options here */
 };
 
 export default nextConfig;
