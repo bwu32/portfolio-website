1d537e04a8359f4a489410eb3321df797a9db392
bwu32 <bwu32@terpmail.umd.edu>
2026-01-15 02:38:40 -0500

    hi

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone', // <--- This creates the ultra-fast production build
-  /* your other config options here */
+  output: 'standalone',
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
