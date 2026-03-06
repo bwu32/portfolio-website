d86d2ccff1729c439fa14fae03a7d8bb1e8846e5
brian wu <bwu32@terpmail.umd.edu>
2026-03-06 14:33:44 -0500

    time

--- src/app/legos/LegoContent.tsx
+++ src/app/legos/LegoContent.tsx
@@ -12,6 +12,7 @@ export default function LegoContent() {
     const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
     const legoSets = [
+        { image: "/icons/legos/delorean.png", date: "Jan 1 2026", description: "Back to the Future Time Machine" },
         { image: "/icons/legos/lightningmcqueen.png", date: "Jan 1 2026", description: "Lightning McQueen" },
         { image: "/icons/legos/mclarenw1.png", date: "Jan 1 2026", description: "McLaren W1" },
         { image: "/icons/legos/sonic.png", date: "Jan 1 2026", description: "Sonic’s Lightning Racer" },
